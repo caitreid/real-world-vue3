9e2a54dcce07f3f2ad85de3fdc76166c5ab289ed
Caitlin Reid <ctlnrd@gmail.com>
2023-05-31 12:16:32 -0400

    initial files

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/EventCard.vue'
+</script>
+
+<template>
+  <div id="layout">
+    <header>
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">Home</RouterLink> |
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+    </header>
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+#layout{
+  color: rgb(87, 80, 81);
+  text-align: center;
+  font-family: Arial;
+}
+nav {
+  padding: 30px;
+}
+nav a {
+  font-weight: bold;
+}
+nav a:hover {
+  color:rgb(112, 188, 255);
+}
+</style>
---
+++ src/components/EventCard.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+defineProps({
+  event: {
+    type: Object,
+    required: true
+  }
+})
+
+</script>
+
+<template>
+    <div class="event-card">
+        <h2>{{ event.title }}</h2>
+        <span>@{{ event.time }} on {{ event.date }}</span>
+    </div>
+</template>
+
+<style scoped>
+.event-card {
+    padding: 2rem;
+    width: 250px;
+    cursor: pointer;
+    border: 1px solid black;
+    margin-bottom: 2rem;
+}
+.event-card:hover {
+    transform: scale(1.01);
+    box-shadow: 0 3px 12px 0 rgba(0,0,0,0.2)
+}
+</style>
